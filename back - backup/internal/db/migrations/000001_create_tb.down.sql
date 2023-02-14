0210a5512ee6b2826bf89edbdd8ae82599401a13
carlosabdoamaraldb1 <v-carlos.amaral@avenuesec.io>
2023-02-14 19:30:29 -0300

    removing useless files

--- back - backup/internal/db/migrations/000001_create_tb.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE car_tb;
-DROP TABLE discount_tier_tb;
-DROP TABLE parking_lot_tb;
--- mobile/android/app/src/main/kotlin/com/example/valet_solution/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.valet_solution
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
