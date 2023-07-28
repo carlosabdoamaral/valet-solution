8f82868bcdd01ce8307b1d8d2655622b783acae8
Carlos Amaral <72256470+carlosabdoamaral@users.noreply.github.com>
2023-07-28 13:29:29 -0300

    Removing old frontend

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-import { Controller } from "./utils/controller";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Controller />);
-
-reportWebVitals();
--- frontend/templates/service.js
+++
@@ -1,9 +0,0 @@
-export default class Service {
-  constructor() {
-    // code
-  }
-
-  someFunc() {
-    // code
-  }
-}
